a592fe7f209bca01a1e2a5458f5fb9f80bd2e239
Tej749 <jargha261975@gmail.com>
2024-09-17 16:59:00 +0545

    navbar

--- npjTelecom/views.py
+++ npjTelecom/views.py
@@ -8,3 +8,13 @@ def index(request):
     return render(request, "npjTelecom/index.html", {'data':data})
 
 
+def contact(request):
+    return render(request, "npjTelecom/contact.html")
+
+def about(request):
+    return render(request, "npjTelecom/about.html")
+
+def service(request):
+    return render(request, "npjTelecom/service.html")
+
+
